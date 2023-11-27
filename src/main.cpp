40b7a06f9de4ba9a9eaa6745fefb8b19543884e4
marialemor <m.morenob@uniandes.edu.co>
2023-11-27 15:52:09 -0500

    Ya no muere

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,46 @@
 #include <Arduino.h>
+
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "Adafruit_MPU6050.h"
-#include <WiFi.h>
-#include "time.h"
+#include <SPI.h>
 
-//botones con el numero de pin 
-const int pinBotonWalk = 2;
-const int pinBotonBath = 3;
-const int pinBotonEat = 3;
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/semphr.h>
+#include <freertos/queue.h>
 
 QueueHandle_t queueBotton;
 
-Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
+//parametros de la pantalla
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64 
+
+#define OLED_RESET     -1 
+#define SCREEN_ADDRESS 0x3C 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//parametros del MPU
 Adafruit_MPU6050 mpu;
 
+//pines de los botones
+const int BathButton = 18;
+const int EatButton = 5;
+const int WalkButton = 19;
+
+void Pant(void *pvParameters);
+void Eat(void *pvParameters);
+void walk(void *pvParameters);
+void Bath(void *pvParameters);
+void time_count(void *pvParameters);
+void Barras(void *pvParameters);
+
+#define WIRE Wire
+
+//parametros para caminar
 int pasos = 0;
 int iniciar = 0; 
 int contar = 0;
@@ -27,17 +50,15 @@ float hunger = 100;
 float fun = 100;
 float bath = 100;
 
-const char* ssid       = "Mariayo";
-const char* password   = "M4R14L3J4";
-
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 3600;
-const int   daylightOffset_sec = 3600;
-
 void Pant(void *pvParameters){
 
   while(1) {
-    
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println("MENU");
+    display.display();
+    delay(2000);
+
   }
 }
 
@@ -86,10 +107,13 @@ void Eat(void *pvParameters){
 
     if (xQueueReceive(queueBotton,&buttonReceived,portMAX_DELAY)){
       if (buttonReceived == 2){
+        display.setCursor(0,0);
+        display.println("eat");
+        display.display();
+        display.clearDisplay();
         //task
       }
     }
-  
   }
 }
 
@@ -99,53 +123,64 @@ void Bath(void *pvParameters){
   while(1) {
     if (xQueueReceive(queueBotton,&buttonReceived,portMAX_DELAY)){
       if (buttonReceived == 3){
-        //task
+        display.setCursor(0,0);
+        display.println("Bath");
+        display.display();
+        display.clearDisplay();
       }
     }
 
   }
 }
 
-void Botton(void *pvParameters){
-
-  while(1) {
-    if (digitalRead(pinBotonBath) == LOW) {
-      int dataButton = 1;
-      xQueueSend(queueBotton,&dataButton,  portMAX_DELAY);
-
-    }
-
-    else if (digitalRead(pinBotonEat) == LOW) {
-      int dataButton = 2;
-      xQueueSend(queueBotton,&dataButton,  portMAX_DELAY);
-    
-    }
-
-    else if (digitalRead(pinBotonWalk) == LOW) {
-      int dataButton = 3;
-      xQueueSend(queueBotton,&dataButton,  portMAX_DELAY);
-    }
-
+void Eat_Interrupt()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > 500)
+  {
+      int dataEat = 1;
+      xQueueSend(queueBotton, &dataEat, portMAX_DELAY);
   }
+  last_interrupt_time = interrupt_time;
 }
 
-void time_count(void *pvParameters) {
-  (void)pvParameters;
-  while(1) {
-    //init and get the time
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-    struct tm timeinfo;
-    if(!getLocalTime(&timeinfo)){
-      Serial.println("Failed to obtain time");
-      return;
-    }
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+void Walk_Interrupt()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > 500)
+  {
+      int dataWalk = 2;
+      xQueueSend(queueBotton, &dataWalk, portMAX_DELAY);
     
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+void Bath_Interrupt()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > 500)
+  {
+      int dataBath = 3;
+      xQueueSend(queueBotton, &dataBath, portMAX_DELAY);
+    
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+
+void time_count(void *pvParameters) {
+  
+  while(1) {
+    Serial.print("time");
   }
 }
 
 void Barras(void *pvParameters) {
-  (void)pvParameters;
+  
   while(1) {
     //task
   }
@@ -154,103 +189,50 @@ void Barras(void *pvParameters) {
 void setup() {
   Serial.begin(9600);
 
-
-  queueBotton = xQueueCreate(1,sizeof(3));
-
-
   // Initialize the display
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-
-  // Clear the display buffer
-  display.clearDisplay();
-  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
-  delay(500);
+  delay(200);
+  display.setRotation(1);
+  display.clearDisplay();
   display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setRotation(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("INIT");
+  display.display();
+  delay(200);
+
+  //pines de los botones
+
+  pinMode(WalkButton, INPUT_PULLUP);
+  pinMode(BathButton, INPUT_PULLUP);
+  pinMode(EatButton, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(WalkButton), Walk_Interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BathButton), Bath_Interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(EatButton), Eat_Interrupt, FALLING);
 
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
   }
+  
+  Serial.println("MPU6050 Found!");
+
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
 
-    //connect to WiFi
-  Serial.printf("Connecting to %s ", ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println(" CONNECTED");
+  //Cola para botones
+  queueBotton = xQueueCreate(20,sizeof(int));
 
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              Pant,  // Function to be called
-              "Inicializar Pantalla",   // Name of task
-              4096,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              NULL);     // Run on one core for demo purposes (ESP32 only)
-  
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              walk,  // Function to be called
-              "Walk",   // Name of task
-              4096,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              NULL);     // Run on one core for demo purposes (ESP32 only)
+  //Creacion de los tasks
+  xTaskCreate(Pant," Pantalla", 4096, NULL, 1, NULL);  
+  xTaskCreate(Eat," Eat", 4096, NULL, 1, NULL); 
+  xTaskCreate(walk," walk", 4096, NULL, 1, NULL); 
+  xTaskCreate(Bath," Bath", 4096, NULL, 1, NULL); 
+  xTaskCreate(Barras," Barras", 4096, NULL, 1, NULL); 
 
-xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              Eat,  // Function to be called
-              "Eat",   // Name of task
-              4096,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              NULL);     // Run on one core for demo purposes (ESP32 only)
-
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              Bath,  // Function to be called
-              "Bath",   // Name of task
-              4096,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              NULL);     // Run on one core for demo purposes (ESP32 only)
-
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              Botton,  // Function to be called
-              "Botones",   // Name of task
-              4096,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              NULL);     // Run on one core for demo purposes (ESP32 only)
-
-    xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              Barras,  // Function to be called
-              "Barras",   // Name of task
-              4096,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              NULL);     // Run on one core for demo purposes (ESP32 only)
-
-  // If this was vanilla FreeRTOS, you'd want to call vTaskStartScheduler() in
-  // main after setting up your tasks.
+  vTaskStartScheduler();
 }
 
 void loop() {
-  // Do nothing
-  // setup() and loop() run in their own task with priority 1 in core 1
-  // on ESP32
 }
